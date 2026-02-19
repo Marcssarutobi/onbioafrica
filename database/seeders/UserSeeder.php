8180dded8b675ef7dbef0362043bc13e0fbf0d29
hstkbj <hstkbj@asiso.io>
2026-02-19 17:56:41 +0100

    feat: ajouter le sélecteur d'utilisateur et la page de connexion pour l'authentification des administrateurs

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'full_name' => 'Superadmin',
+                'email' => 'superadmin@onebioafrica.com',
+                'role' => 'superadmin',
+                'password' => 'onebioafricaSuper@2025',
+            ],
+            [
+                'full_name' => 'Admin',
+                'email' => 'admin@onebioafrica.com',
+                'role' => 'admin',
+                'password' => 'onebioafricaAdmin@2025',
+            ],
+        ];
+
+        foreach ($users as $user) {
+
+            DB::table('users')->insert([
+                'full_name' => $user['full_name'],
+                'email' => $user['email'],
+                'password' => Hash::make($user['password']), // personnalisé
+                'role' => $user['role'],
+                'is_active' => true,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
